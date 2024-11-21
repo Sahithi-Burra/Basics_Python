27843a4903746d703fe6b7aeed5d33cedae99af9
Sahithi-Burra <sahithiburra2004@gmail.com>
2024-11-21 18:51:31 +0530

    Add files via upload
    i have used different conditional statements for dividing people based on there marks for a specified grade.

---
+++ conditionstmts.py
@@ -0,0 +1,11 @@
+name=(input("enter your name:"))
+marks=int(input("enter your marks:"))
+if(marks>=90):
+    print("A grade")
+elif(80<= marks<=89):
+    print("B grade")
+elif(70<=marks<=79):
+    print("C grade")
+else:
+    print("fail")
+    
